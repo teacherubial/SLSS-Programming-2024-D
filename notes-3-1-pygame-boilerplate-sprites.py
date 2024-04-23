6cbc491b638dfcb958b9ebad9626ce32c9c7de97
Tim Ubial <tubial@sd38.bc.ca>
2024-04-23 14:45:20 -0700

    added dvd logo

---
+++ notes-3-1-pygame-boilerplate-sprites.py
@@ -0,0 +1,86 @@
+# Introduction to Pygame
+#    - boilerplate is useful to set up our environments
+#    - the Sprite class has some really cool things in it
+
+import pygame
+
+
+class Dvdlogo(pygame.sprite.Sprite):
+    """Represents DVD logo sprite"""
+
+    def __init__(self):
+        super().__init__()
+
+        # image -> visual representation
+        self.image = pygame.image.load("./Images/dvd-logo.png")
+
+        # rect  -> hitbox representation
+        # get_rect() -> Rect
+        #     [x, y, width, height]
+        #     [0, 0, <width of image>, <height of image>]
+        self.rect = self.image.get_rect()
+
+
+def start():
+    """Environment Setup and Game Loop"""
+
+    pygame.init()
+
+    # --CONSTANTS--
+    # COLOURS
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    EMERALD = (21, 219, 147)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    GRAY = (128, 128, 128)
+
+    WIDTH = 1280  # Pixels
+    HEIGHT = 720
+    SCREEN_SIZE = (WIDTH, HEIGHT)
+
+    # --VARIABLES--
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    done = False
+    clock = pygame.time.Clock()
+
+    pygame.display.set_caption("DVD Logo Screensaver")
+
+    # Make a DVD Logo object
+    dvdlogo = Dvdlogo()
+
+    # Create a group of sprites
+    all_sprites = pygame.sprite.Group()
+
+    # Add the DVD Logo object to the group of sprites
+    all_sprites.add(dvdlogo)
+
+    # --MAIN LOOP--
+    while not done:
+        # --- Event Listener
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # --- Update the world state
+
+        # --- Draw items
+        screen.fill(BLACK)
+
+        # Draw all the sprites on the screen
+        all_sprites.draw(screen)
+
+        # Update the screen with anything new
+        pygame.display.flip()
+
+        # --- Tick the Clock
+        clock.tick(60)  # 60 fps
+
+
+def main():
+    start()
+
+
+if __name__ == "__main__":
+    main()
