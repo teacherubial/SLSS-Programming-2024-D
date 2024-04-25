3bd066360549ecadc4dbea6c064ba249492127fe
Tim Ubial <tubial@sd38.bc.ca>
2024-04-25 13:31:06 -0700

    moving dvd image

--- notes-3-1-pygame-boilerplate-sprites.py
+++ notes-3-1-pygame-boilerplate-sprites.py
@@ -20,6 +20,10 @@ class Dvdlogo(pygame.sprite.Sprite):
         #     [0, 0, <width of image>, <height of image>]
         self.rect = self.image.get_rect()
 
+        # Velocity of the Dvd Logo
+        self.vel_x = 3
+        self.vel_y = 3
+
 
 def start():
     """Environment Setup and Game Loop"""
@@ -50,6 +54,10 @@ def start():
     # Make a DVD Logo object
     dvdlogo = Dvdlogo()
 
+    # Move the dvd logo to the middle
+    dvdlogo.rect.centerx = WIDTH // 2
+    dvdlogo.rect.centery = HEIGHT // 2
+
     # Create a group of sprites
     all_sprites = pygame.sprite.Group()
 
@@ -64,6 +72,13 @@ def start():
                 done = True
 
         # --- Update the world state
+        # Update the location of the dvdlogo
+        #     dvdlogo.rect.x + dvdlogo.vel_x
+        #     dvdlogo.rect.y + dvdlogo.vel_y
+        dvdlogo.rect.x = dvdlogo.rect.x + dvdlogo.vel_x
+        dvdlogo.rect.y += dvdlogo.vel_y
+
+        print(dvdlogo.rect.x, dvdlogo.rect.y)
 
         # --- Draw items
         screen.fill(BLACK)
